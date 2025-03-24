afd22a13702b8de223505dcdfd584b6fc80b1d87
Phong Vu <phongvue4@gmail.com>
2025-03-24 10:42:41 +0700

    them nhiu question cung luc

--- TruthOrDare_Contract/IRepository/IQuestionRepository.cs
+++ TruthOrDare_Contract/IRepository/IQuestionRepository.cs
@@ -14,5 +14,6 @@ namespace TruthOrDare_Contract.IRepository
         Task<int> GetPointsForQuestionAsync(string questionId);
         Task<string> CreateQuestion(QuestionCreateDTO question);
         Task<List<Question>> GetQuestions(string? mode, string? type, string? difficulty, string? age_group);
+        Task<(int SuccessCount, List<string> Errors)> InsertManyQuestions(List<QuestionCreateDTO> questions);
     }
 }
