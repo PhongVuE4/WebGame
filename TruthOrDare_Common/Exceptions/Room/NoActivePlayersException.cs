da8f878239dc53f026f20e8a2e7bafdd55b24073
Phong Vu <phongvue4@gmail.com>
2025-04-16 21:31:56 +0700

    upd playerCount

---
+++ TruthOrDare_Common/Exceptions/Room/NoActivePlayersException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruthOrDare_Common.Exceptions.Room
+{
+    public class NoActivePlayersException : Exception
+    {
+        public NoActivePlayersException() : base("No active players in the room.")
+        {
+        }
+    }
+}
